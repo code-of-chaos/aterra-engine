618aaad3c617a1c4c7a91d5f0ae4c3cf0e642baa
DirectiveAthena <directive.athena@gmail.com>
2023-10-04 17:01:11 +0200

    Feat: Bring-over

---
+++ AterraUnleashed/AterraUnleashed/Program.cs
@@ -0,0 +1,77 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using System.Numerics;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace ConsoleApp1; 
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class JsonData
+{
+    public List<List<int>> tile_map { get; set; }
+    public JsonData(List<List<int>> tile_map) {
+        this.tile_map = tile_map;
+    }
+}
+internal static class Program {
+    private static void Main() {
+        // get data from json file
+        string json_data = File.ReadAllText("E:\\Syntra\\syntra-23_24-cs_development\\projects\\00-learning\\Solution1\\ConsoleApp1\\map_data.json");
+        
+        // Deserialize the JSON data into the JsonData class
+        JsonData? jsonData = JsonSerializer.Deserialize<JsonData>(json_data);
+        if (json_data == null) {
+            throw new Exception("Json file wasn't configured properly");
+        }
+        
+        // Extract the tile_map property
+        List<List<int>> tile_map = jsonData.tile_map;
+        
+        // Store tile placement commands in a list
+        var world_rows = tile_map.Count;
+        var world_cols = tile_map[0].Count;
+        
+        var world = new World(
+            world_size_x:world_rows,
+            world_size_y:world_cols
+        );
+
+        int x;
+        int y;
+        for (x = 0; x < world_rows; x++) {
+            for (y = 0; y < world_cols; y++) {
+                int tileTypeNum = tile_map[x][y];
+
+                // Get the correct TileTypes based on the integer value
+                TileTypes tileType = (TileTypes)tileTypeNum;
+
+                world.add_tile_to_grid(new Vector2(x, y), new Tile(tileType));
+            }
+        }
+
+        world.print_grid();
+
+        Console.ReadLine();
+        
+        world.spawn_player();
+        world.print_grid();
+
+        Console.WriteLine("x_offset");
+        string? input = Console.ReadLine();
+        int.TryParse(input, out var x_);
+        
+        Console.WriteLine("y_offset");
+        input = Console.ReadLine();
+        int.TryParse(input, out var y_);
+
+        
+        world.move_player(new Vector2(x_, y_));
+        world.print_grid();
+        
+    }
+}
---
+++ AterraUnleashed/AterraUnleashed/TileTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1; 
+
+public enum TileTypes {
+    Void,
+    Water,
+    Grassland,
+    Stone,
+    City
+}
+
+public class TileTypePrintout {
+    private static readonly Dictionary<TileTypes, string> _printout = new Dictionary<TileTypes, string>() {
+        {TileTypes.Void , "  "},
+        {TileTypes.Water, "~~"},
+        {TileTypes.Grassland, ".."},
+        {TileTypes.Stone, "/\\"},
+        { TileTypes.City, "[]"}
+    };
+
+    public static string get_printout(TileTypes tile_type) {
+        return _printout.GetValueOrDefault(tile_type, "  ");
+    }
+}
