6009569e072aaba46b18880960c5483c28e1ae8e
AndreasSasDev <directive.athena@gmail.com>
2023-11-03 18:25:31 +0100

    Feat: more vars to class body

--- AterraEngine/Engine/Renderer/EngineRenderer.cs
+++ AterraEngine/Engine/Renderer/EngineRenderer.cs
@@ -14,20 +14,19 @@ namespace AterraEngine.Engine.Renderer;
 public class EngineRenderer:IEngineRenderer {
     private readonly ILogger _logger = EngineServices.getLogger();
     
-    private const int _frame_size = 15;
+    private const int _frame_size = 31;
     private readonly IChunk?[,] _camera_view = new IChunk?[_frame_size, _frame_size];
     private readonly int _chunk_size = EngineServices.getDEFAULTS().chunk_max_size;
-    private const string blank_str = "-";
-
+    private const string _blank_str = "-";
+    private static readonly int _lbound = -(int)Math.Floor(_frame_size / 2f);
+    // private static readonly int ubound =  (int)Math.Ceiling(_frame_size / 2f);
+    
     public void renderFrame(ILevel current_level, IPosition2D camera_pos) {
-        int lbound = -(int)Math.Floor(_frame_size / 2f);
-        // int ubound =  (int)Math.Ceiling(_frame_size / 2f);
-        
         // Gather chunks for the camera to view
         for (int i = 0; i < _frame_size; i++) {
             for (int j = 0; j < _frame_size; j++) {
                 current_level.tryGetChunk(
-                    new Position2D(camera_pos.X + i + lbound, camera_pos.Y + j + lbound),
+                    new Position2D(camera_pos.X + i + _lbound, camera_pos.Y + j + _lbound),
                     out _camera_view[i, j]
                 );
             }
@@ -40,7 +39,7 @@ public class EngineRenderer:IEngineRenderer {
                 for (    int n_col = 0;       n_col < _frame_size;        n_col++) {
                     for (int n_chunk_col = 0; n_chunk_col < _chunk_size; n_chunk_col++) {
                         
-                        string text = _camera_view[n_row, n_col]?.tile_map[n_chunk_row, n_chunk_col]?.console_text ?? blank_str;
+                        string text = _camera_view[n_row, n_col]?.tile_map[n_chunk_row, n_chunk_col]?.console_text ?? _blank_str;
                         Console.Out.Write($"{text}{text}");
                     }
                 }
